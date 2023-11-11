6ff074d2cf03f6bf50e24a94af3d40e84642139b
cdelro28 <149708117+cdelro28@users.noreply.github.com>
2023-11-10 23:45:42 -0500

    fixed JS prevent default issue
    lesson learned: submit event to be applied to the form (input & button) not just the button.

--- src/script.js
+++ src/script.js
@@ -1,15 +1,8 @@
-alert(
-  "This pops up when the page is refreshed. the prevent default code is not working. This page is still in a working progress.Thank you for visiting my page"
-);
-
 function applySearch(event) {
-  event.preventDefault(); // why is this not working? There is no console issues.
+  event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  console.log(searchInputElement.value);
   let searchCityElement = document.querySelector("#search-city");
   searchCityElement.innerHTML = searchInputElement.value;
-  console.log(searchCityElement);
 }
-
-let searchButton = document.querySelector("#search-button");
-searchButton.addEventListener("submit", applySearch);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", applySearch);
