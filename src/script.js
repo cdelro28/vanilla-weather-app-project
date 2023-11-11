d3fb09155755395e903447715be13695e361a17c
cdelro28 <149708117+cdelro28@users.noreply.github.com>
2023-11-11 00:49:41 -0500

    All weather data now linked to API for live updates
    metric units only at this point in time

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
 function updateWeatherData(response) {
   let tempValueElement = document.querySelector("#temp-value");
   let cityElement = document.querySelector("#search-city");
+  let decriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
   tempValueElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  decriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 function searchCity(city) {
