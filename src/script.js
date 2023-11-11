e1e6f6842e69f9bb8652a803dd5588f20beb5559
cdelro28 <149708117+cdelro28@users.noreply.github.com>
2023-11-11 00:09:33 -0500

    added API

--- src/script.js
+++ src/script.js
@@ -1,8 +1,19 @@
+function updateWeatherData(response) {
+  console.log(response);
+}
+
+function searchCity(city) {
+  let apiKey = "ba054cd6f15a15c42o3b84c5df16bbtf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function applySearch(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let searchCityElement = document.querySelector("#search-city");
   searchCityElement.innerHTML = searchInputElement.value;
+  searchCity(searchInputElement.value);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", applySearch);
