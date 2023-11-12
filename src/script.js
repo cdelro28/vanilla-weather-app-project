47881d45c25c38f2771828cac114307185f6baa0
cdelro28 <149708117+cdelro28@users.noreply.github.com>
2023-11-12 00:22:59 -0500

    added live time using API JS
    Time is according to eastern time only. Not sure how to change time to set at local time zone.

--- src/script.js
+++ src/script.js
@@ -4,11 +4,38 @@ function updateWeatherData(response) {
   let decriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let windSpeedApiRound = Math.round(response.data.wind.speed * 10) / 10;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  timeElement.innerHTML = formatDate(date);
   tempValueElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   decriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  windSpeedElement.innerHTML = `${windSpeedApiRound}km/h`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let weekDays = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = weekDays[date.getDay()];
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes} `;
 }
 
 function searchCity(city) {
